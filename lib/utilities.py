0f3c7b46cf6682dfc51d18e4e37ba09e48b2face
Xiaotao Wang <wangxiaotao868@163.com>
2018-05-21 14:10:22 +0800

    utilities.toCooler

--- lib/utilities.py
+++ lib/utilities.py
@@ -11,6 +11,45 @@ from cooler.io import create_from_unordered, sanitize_records, sanitize_pixels
 
 log = logging.getLogger(__name__)
 
+def _extractChrLabel(filename):
+    
+    """
+    Extract chromosome label from a file name.
+    """
+    # Full filename including path prefix
+    _, interName = os.path.split(filename)
+    regexp = 'chr(.*).txt'
+    search_results = re.search(regexp, filename)
+    label = search_results.group(1)
+    
+    # Remove leading zeroes.
+    if label.isdigit():
+        label = str(int(label))
+    
+    return label
+
+def _scanFolder(folder, chroms):
+    
+    """
+    Create a map from chromosome labels to file names under the folder.
+    """
+    oriFiles = glob.glob(os.path.join(folder, 'chr*.txt'))
+    
+    # Read chromosome labels
+    labels = []
+    interFiles = [] # Depend on user's selection
+    for i in oriFiles:
+        label = _extractChrLabel(i)
+        if ((not chroms) or (label.isdigit() and '#' in chroms) or (label in chroms)):
+            labels.append(label)
+            interFiles.append(i)
+        
+    # Map from labels to files
+    Map = dict(zip(labels, interFiles))
+        
+    return Map
+    
+
 def toCooler(outfil, data_path, resolution, chroms=['#','X'], cache_dir=None,
             delete_cache=True):
     """
@@ -46,4 +85,35 @@ def toCooler(outfil, data_path, resolution, chroms=['#','X'], cache_dir=None,
     
     
     """
+    outfil = os.path.abspath(os.path.expanduser(outfil))
+    if os.path.exists(outfil):
+        log.error('Cooler file {} already exists, exit ...')
+        sys.exit(1)
+        
+    ## ready for data loading
+    tl = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+    kw = {'prefix':'bins', 'suffix':tl, 'dir':cache_dir}
+    bin_fil = tempfile.mktemp(**kw)
+    kw = {'prefix':'pixels', 'suffix':tl, 'dir':cache_dir}
+    pixel_fil = tempfile.mktemp(**kw)
+    pool = {}
+    chromlist = []
+    if data_path.endswith('.npz'):
+        lib = np.load(data_path)
+        for i in lib.files:
+            if ((not chroms) or (i.isdigit() and '#' in chroms) or (i in chroms)):
+                 chromlist.append(i)
+    else:
+        Map = _scanFolder(data_path)
+        for i in Map:
+            chromlist.append(i)
     
+    # sort chromosome labels
+    tmp = map(str, sorted(map(int, [i for i in chromlist if i.isdigit()])))
+    nondigits = [i for i in chromlist if not i.isdigit()]
+    for i in ['X','Y','M']:
+        if i in nondigits:
+            tmp.append(nondigits.pop(nondigits.index(i)))
+    chromlist = tmp + sorted(nondigits)
+    
+        
