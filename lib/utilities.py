0b38df54cc988479b96ca381f2993c26b378015c
Xiaotao Wang <wangxiaotao868@163.com>
2018-05-27 23:42:10 +0800

    cooler from TXT finished

--- lib/utilities.py
+++ lib/utilities.py
@@ -5,55 +5,19 @@ Created on Sat May 19 16:00:16 2018
 @author: XiaoTao Wang
 """
 
-import glob, re, os, sys, cPickle, tempfile, time, zipfile, logging
+import os, sys, tempfile, time, logging
 import numpy as np
-from cooler.io import create_from_unordered, sanitize_records, sanitize_pixels
+import pandas as pd
+from cooler.util import binnify
+from cooler.io import create_from_unordered, sanitize_records
 
 log = logging.getLogger(__name__)
-
-def _extractChrLabel(filename):
-    
-    """
-    Extract chromosome label from a file name.
-    """
-    # Full filename including path prefix
-    _, interName = os.path.split(filename)
-    regexp = 'chr(.*).txt'
-    search_results = re.search(regexp, filename)
-    label = search_results.group(1)
-    
-    # Remove leading zeroes.
-    if label.isdigit():
-        label = str(int(label))
-    
-    return label
-
-def _scanFolder(folder, chroms):
-    
-    """
-    Create a map from chromosome labels to file names under the folder.
-    """
-    oriFiles = glob.glob(os.path.join(folder, 'chr*.txt'))
-    
-    # Read chromosome labels
-    labels = []
-    interFiles = [] # Depend on user's selection
-    for i in oriFiles:
-        label = _extractChrLabel(i)
-        if ((not chroms) or (label.isdigit() and '#' in chroms) or (label in chroms)):
-            labels.append(label)
-            interFiles.append(i)
-        
-    # Map from labels to files
-    Map = dict(zip(labels, interFiles))
-        
-    return Map
     
 
-def toCooler(outfil, data_path, resolution, chroms=['#','X'], cache_dir=None,
-            delete_cache=True):
+def toCooler(outfil, data_path, res, assembly, chroms=['#','X'], symmetric=True,
+             count_type=float, cache_dir=None, delete_cache=True):
     """
-    Create a Cooler from TXT or NPZ format Hi-C data.
+    Create a Cooler from TXT Hi-C data.
     
     Parameters
     ----------
@@ -61,13 +25,10 @@ def toCooler(outfil, data_path, resolution, chroms=['#','X'], cache_dir=None,
         Path of the output Cooler file.
         
     data_path : str
-        If your Hi-C data are stored in *NPZ* format, this string should point
-        to the npz file. Otherwise in the *TXT* format case, data from different
-        chromosomes must be stored separately and placed within a same folder,
-        and this string should point to the path of the folder.
+        Path of original contact matrix file (in TXT format).
         
-    resolution : int
-        Resolution / Bin size of the Hi-C data in base pairs.
+    res : int
+        Resolution / Bin size of the matrix in base pairs.
     
     chroms : list
         List of chromosome labels. Only Hi-C data within the specified chromosomes
@@ -81,7 +42,7 @@ def toCooler(outfil, data_path, resolution, chroms=['#','X'], cache_dir=None,
         used. (Default: None)
     
     delete_cache : Bool
-        Whether to delete temporary files when finished.
+        Whether to delete temporary files when finished. (Default: True)
     
     
     """
@@ -92,22 +53,37 @@ def toCooler(outfil, data_path, resolution, chroms=['#','X'], cache_dir=None,
         
     ## ready for data loading
     tl = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-    kw = {'prefix':'bins', 'suffix':tl, 'dir':cache_dir}
-    bin_fil = tempfile.mktemp(**kw)
     kw = {'prefix':'pixels', 'suffix':tl, 'dir':cache_dir}
     pixel_fil = tempfile.mktemp(**kw)
-    pool = {}
-    chromlist = []
-    if data_path.endswith('.npz'):
-        lib = np.load(data_path)
-        for i in lib.files:
-            if ((not chroms) or (i.isdigit() and '#' in chroms) or (i in chroms)):
-                 chromlist.append(i)
-    else:
-        Map = _scanFolder(data_path)
-        for i in Map:
-            chromlist.append(i)
     
+    # write the pixel file
+    chromsizes = {}
+    with open(pixel_fil, 'wb') as out:
+        with open(data_path, 'rb') as source:
+            for line in source:
+                c1, p1, c2, p2, count = line.rstrip().split()
+                c1 = c1.lstrip('chr')
+                c2 = c2.lstrip('chr')
+                check1 = ((not chroms) or (c1.isdigit() and ('#' in chroms)) or (c1 in chroms))
+                check2 = ((not chroms) or (c2.isdigit() and ('#' in chroms)) or (c2 in chroms))
+                if (not check1) or (not check2):
+                    continue
+                ip_1, ip_2 = int(p1)+res, int(p2)+res
+                if c1 in chromsizes:
+                    if ip_1 > chromsizes[c1]:
+                        chromsizes[c1] = ip_1
+                else:
+                    chromsizes[c1] = ip_1
+                if c2 in chromsizes:
+                    if ip_2 > chromsizes[c2]:
+                        chromsizes[c2] = ip_2
+                else:
+                    chromsizes[c2] = ip_2
+                newline = [c1, p1, str(ip_1), c2, p2, str(ip_2), count]
+                out.write('\t'.join(newline)+'\n')
+    
+    # generate bin table
+    chromlist = chromsizes.keys()
     # sort chromosome labels
     tmp = map(str, sorted(map(int, [i for i in chromlist if i.isdigit()])))
     nondigits = [i for i in chromlist if not i.isdigit()]
@@ -115,7 +91,53 @@ def toCooler(outfil, data_path, resolution, chroms=['#','X'], cache_dir=None,
         if i in nondigits:
             tmp.append(nondigits.pop(nondigits.index(i)))
     chromlist = tmp + sorted(nondigits)
+    lengths = [chromsizes[i] for i in chromlist]
+    chromsizes = pd.Series(data=lengths, index=chromlist)
+    bins = binnify(chromsizes, res)
     
-    # write the bin table
+    # output fields
+    output_field_names = ['bin1_id', 'bin2_id', 'count']
+    output_field_dtypes = {'bin1_id': int, 'bin2_id': int, 'count': count_type}
+    
+    # input fields
+    input_field_names = ['chrom1', 'start1', 'end1', 'chrom2', 'start2', 'end2', 'count']
+    input_field_dtypes = {
+            'chrom1': str, 'start1': int, 'end1': int,
+            'chrom2': str, 'start2': int, 'end2': int,
+            'count': count_type,
+    }
+    input_field_numbers = {
+            'chrom1': 0, 'start1': 1, 'end1': 2,
+            'chrom2': 3, 'start2': 4, 'end2': 5,
+            'count': 6,
+    }
+    
+    tril_action = 'drop' if symmetric else 'reflect'
+    pipeline = sanitize_records(bins, schema='bg2', is_one_based=False,
+                                tril_action=tril_action, sort=True)
+    
+    reader = pd.read_table(
+                    pixel_fil,
+                    usecols=[input_field_numbers[name] for name in input_field_names],
+                    names=input_field_names,
+                    dtype=input_field_dtypes,
+                    iterator=True,
+                    chunksize=int(40e6))
+    
+    create_from_unordered(
+            outfil,
+            bins,
+            map(pipeline, reader),
+            columns=output_field_names,
+            dtypes=output_field_dtypes,
+            assembly=assembly,
+            mergebuf=int(40e6),
+            ensure_sorted=False)
+    
+    
+    
+    
+    
+    
     
         
