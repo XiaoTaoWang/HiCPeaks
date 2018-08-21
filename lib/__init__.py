8faec284589cf34aca25c0b67559a5e2d7627af5
Xiaotao Wang <wangxiaotao686@gmail.com>
2018-08-20 22:02:26 -0400

    Ready for distribution

--- lib/__init__.py
+++ lib/__init__.py
@@ -6,13 +6,13 @@ import xmlrpclib
 from pkg_resources import parse_version as V
 
 __author__ = 'XiaoTao Wang'
-__version__ = '0.1.0-dev1'
+__version__ = '0.1.0'
 __license__ = 'GPLv3+'
 
 ## Check for update
 try:
     pypi = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
-    available = pypi.package_releases('HiCPeaks')
+    available = pypi.package_releases('hicpeaks')
     if V(__version__) < V(available[0]):
         print '*'*75
         print 'Version %s is out of date, Version %s is available.' % (__version__, available[0])
